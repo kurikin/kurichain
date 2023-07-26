7072dc51439ecf2dcab18db17f6ba4d173d77aec
kurikin <riku.code0309@gmail.com>
2023-07-26 14:17:19 +0900

    Add add & subtract method

--- ecc.py
+++ ecc.py
@@ -17,3 +17,15 @@ class FieldElement:
 
     def __ne__(self, other):
         return not (self == other)
+
+    def __add__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot add two numbers in different Fields')
+        num = (self.num + other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __sub__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot subtract two numbers in different Fields')
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)

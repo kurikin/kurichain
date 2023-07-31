c57a890c99c3c923080eec4497e363a634c0d7fa
kurikin <riku.code0309@gmail.com>
2023-07-31 23:08:34 +0900

    Fix method name

--- ecc_test.py
+++ ecc_test.py
@@ -3,6 +3,26 @@ import unittest
 
 class ECCTest(unittest.TestCase):
 
+    def test_ex01(self):
+        prime = 223
+
+        def on_curve(x, y) -> bool:
+            a = FE(0, prime)
+            b = FE(7, prime)
+            return y**2 == x**3 + a*x + b
+
+        x1, y1 = FE(192, prime), FE(105, prime)
+        x2, y2 = FE(17, prime), FE(56, prime)
+        x3, y3 = FE(200, prime), FE(119, prime)
+        x4, y4 = FE(1, prime), FE(193, prime)
+        x5, y5 = FE(42, prime), FE(99, prime)
+
+        self.assertEqual(on_curve(x1, y1), True)
+        self.assertEqual(on_curve(x2, y2), True)
+        self.assertEqual(on_curve(x3, y3), False)
+        self.assertEqual(on_curve(x4, y4), True)
+        self.assertEqual(on_curve(x5, y5), False)
+
     def test_on_curve(self):
         prime = 223
         a = FE(0, prime)
@@ -20,7 +40,7 @@ class ECCTest(unittest.TestCase):
             with self.assertRaises(ValueError):
                 Point(x, y, a, b)
 
-    def test_add(self):
+    def test_ex03(self):
         prime = 223
         a = FE(0, prime)
         b = FE(7, prime)
